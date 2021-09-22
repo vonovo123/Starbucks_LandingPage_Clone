9f814e46290be052ceb1288ffe6e3e57b3d21523
dynamic-henry-kwon <jukdo67@gmail.com>
2021-09-22 15:22:27 +0900

    header fin

--- src/metarials/js/main.js
+++ src/metarials/js/main.js
@@ -16,3 +16,23 @@ searchInputEl.addEventListener('blur', (e) => {
   searchEl.classList.remove('focused')
   searchInputEl.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(() => {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6,
+      {
+        opacity: 0,
+        display: 'none'
+      }
+    );
+  } else {
+    gsap.to(badgeEl, .6,
+      {
+        opacity: 1,
+        display: 'block'
+      }
+    )
+  }
+}, 300));
