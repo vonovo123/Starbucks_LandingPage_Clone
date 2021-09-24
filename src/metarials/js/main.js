7c0e07d92483acb4497c0a2fd749814e6207e516
dynamic-henry-kwon <jukdo67@gmail.com>
2021-09-24 20:31:55 +0900

    scroll animation & slide

--- src/metarials/js/main.js
+++ src/metarials/js/main.js
@@ -65,6 +65,18 @@ new Swiper('.promotion .swiper', {
   }
 })
 
+new Swiper('.awards .swiper',
+  {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+}
+)
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleEl = document.querySelector('.toggle_promotion');
@@ -108,3 +120,4 @@ spyEls.forEach(spyEl => {
     )
     .addTo(new ScrollMagic.Controller());
 })
+
